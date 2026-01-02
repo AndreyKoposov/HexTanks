769b8258afd4519d082d2c7e65f8af6c2dcfab5a
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2026-01-02 20:51:13 +0300

    Units
    - Build panel ccan build every unit

--- Assets/Scripts/UI/BuildingPanel.cs
+++ Assets/Scripts/UI/BuildingPanel.cs
@@ -4,9 +4,12 @@ using UnityEngine.UI;
 
 public class BuildingPanel : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI turnsLeftLabel;
-    [SerializeField] private TextMeshProUGUI unitLabel;
-    [SerializeField] private Button buildButton;
+    [SerializeField] private Button infantryButton;
+    [SerializeField] private Button heavyButton;
+    [SerializeField] private Button scoutButton;
+    [SerializeField] private Button artilleryButton;
+    [SerializeField] private Button defenderButton;
+    [SerializeField] private Button transportButton;
 
     public void Open(Building building)
     {
@@ -17,22 +20,37 @@ public class BuildingPanel : MonoBehaviour
     {
         gameObject.SetActive(false);
 
-        buildButton.onClick.RemoveAllListeners();
+        infantryButton.onClick.RemoveAllListeners();
+        heavyButton.onClick.RemoveAllListeners();
+        scoutButton.onClick.RemoveAllListeners();
+        artilleryButton.onClick.RemoveAllListeners();
+        defenderButton.onClick.RemoveAllListeners();
+        transportButton.onClick.RemoveAllListeners();
     }
 
     private void Setup(Building building)
     {
-        SetTurnsLeftLabel(building.TurnsLeft);
-        SetUnitLabel(building.UnitToBuild);
+        //SetTurnsLeftLabel(building.TurnsLeft);
+        //SetUnitLabel(building.UnitToBuild);
 
-        buildButton.onClick.AddListener(() => building.StartBuildUnit(UnitType.Infantry));
+        infantryButton.onClick.AddListener(() => BuildListener(building, UnitType.Infantry));
+        heavyButton.onClick.AddListener(() => BuildListener(building, UnitType.Heavy));
+        scoutButton.onClick.AddListener(() => BuildListener(building, UnitType.Scout));
+        artilleryButton.onClick.AddListener(() => BuildListener(building, UnitType.Artillery));
+        defenderButton.onClick.AddListener(() => BuildListener(building, UnitType.Defender));
+        transportButton.onClick.AddListener(() => BuildListener(building, UnitType.Transport));
     }
     private void SetTurnsLeftLabel(int turns)
     {
-        turnsLeftLabel.text = $"Turn: {turns}";
+        
     }
     private void SetUnitLabel(UnitType type)
     {
-        unitLabel.text = $"{type}";
+        
+    }
+
+    private void BuildListener(Building building, UnitType type)
+    {
+        building.StartBuildUnit(type);
     }
 }
